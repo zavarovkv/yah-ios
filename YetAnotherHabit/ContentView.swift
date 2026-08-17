705adf9d4c352cc13178d62c9d6c05972e93b9eb
zavarovkv <zavarovkv@gmail.com>
2026-08-17 08:34:49 +0300

    Initial Commit

---
+++ YetAnotherHabit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YetAnotherHabit
+//
+//  Created by kzavarov on 17.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YetAnotherHabit/YetAnotherHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  YetAnotherHabitApp.swift
+//  YetAnotherHabit
+//
+//  Created by kzavarov on 17.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct YetAnotherHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
